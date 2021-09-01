519cbf6a75a202f1a483b2fabb82ab8348d0cdd4
David Ko <david.holy.ko@gmail.com>
2021-08-31 22:21:58 -0400

    update instructions

--- src/Instructions.js
+++ src/Instructions.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Instructions = () => {
   return (
     <div className="instructions">
-      <h4>Modify this code with the follow criteria:</h4>
+      <h4>Modify this code with the following criteria:</h4>
       <ul>
         <li>
           If list is not sorted, "Sort" button should sort words alphabetically
@@ -14,9 +14,9 @@ export const Instructions = () => {
           "Unsort" button should revert to saying "Sort" order
         </li>
         <li>
-          Bonus: add an input form for user to add his/her own words to list.
-          Retain same functionality listed above where original form would have
-          extra words appended
+          Bonus: Add an input form for user to add his/her own words to list. A
+          new word is appended to the unsorted list. When "Unsort" is clicked,
+          the list reverts back to its original order.
         </li>
       </ul>
     </div>
--- src/WordsList.js
+++ src/WordsList.js
@@ -11,8 +11,12 @@ export const WordsList = () => {
     <div className="words-list-container">
       <h2>Words List</h2>
       <ul className="words-list">
-        {DEFAULT_WORDS.map((word, key) => {
-          return <li index={key}>{word}</li>;
+        {DEFAULT_WORDS.map((word, index) => {
+          return (
+            <li key={index} index={index}>
+              {word}
+            </li>
+          );
         })}
       </ul>
       <button onClick={onClick}>Sort</button>
